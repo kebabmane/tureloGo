96d4ba751190c2823c1b1d2f65a1a2e39b6cbeea
kebabman <evans.rhys@gmail.com>
2017-11-30 21:52:47 +1100

    first commit

---
+++ models/feed.go
@@ -0,0 +1,16 @@
+package models
+
+import "github.com/go-ozzo/ozzo-validation"
+
+// Artist represents an artist record.
+type Feed struct {
+	ID       int    `json:"id" db:"id"`
+	FeedName string `json:"feedName" db:"feedName"`
+}
+
+// Validate validates the Artist fields.
+func (m Feed) Validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.FeedName, validation.Required, validation.Length(0, 120)),
+	)
+}
